e8c52dc425e60d220d1f373a040467d61d78fddb
appweb@localhost <hugo.herrera.villalva@gmail.com>
2024-09-20 23:09:46 -0400

    cambios

--- config/init.py
+++ config/init.py
@@ -1,8 +1,3 @@
 TELEGRAM_TOKEN = "7245878740:AAGCGDb-KPwMmsaBZaPWTddbFr0uBmvhAtc"
-# DBUSER = 'admin'
-# DBPASS = 'admin'
-# DBHOST = 'localhost'
-# DB = 'diproavilm'
-
 APIPATH = "http://api.diproavilm.com/?ruta=botrequest"
 
